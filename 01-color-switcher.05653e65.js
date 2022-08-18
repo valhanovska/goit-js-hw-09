b4522c9da7bfd65de0bbceadb758057278d68168
valhanovska <104757214+valhanovska@users.noreply.github.com>
2022-08-18 19:16:53 +0000

    Deploying to gh-pages from @ valhanovska/goit-js-hw-09@2a00dfbfa7170b7e4f3130caf803df517772e254 🚀

---
+++ 01-color-switcher.05653e65.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;t.disabled=!0,e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=e}),1e3)})),t.addEventListener("click",(()=>{clearInterval(d),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.05653e65.js.map
